2684e0bd708a291056b947ffee8738e835931dfb
Ben Davis <bdavis493@byui.edu>
2023-01-18 10:33:16 -0700

    test

--- App.js
+++ App.js
@@ -93,7 +93,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#C3B298',
+    backgroundColor: '#C3B299',
     alignItems: 'center',
     justifyContent: 'center',
   },
